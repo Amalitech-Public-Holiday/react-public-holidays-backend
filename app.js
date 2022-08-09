15259e9167a7e006f1b06612fa9b19c4c1155554
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-09 12:56:33 +0000

    Create a post route for users signup using createUser middleware

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
+const { createUser } = require('./db');
 
 app.use(cors());
 app.use(express.json());
@@ -10,6 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to React Public Holidays</h1>");
 });
+
+app.post('/users/signup', createUser);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
