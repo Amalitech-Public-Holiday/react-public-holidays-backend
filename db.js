a9a69200baef1c464b35dae23f69c9555e78daba
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-09 12:30:07 +0000

    Add a utility function to save new users data and response with status code

--- db.js
+++ db.js
@@ -1,8 +1,20 @@
-const Pool = require('pg').Pool;
+const Pool = require("pg").Pool;
 const db = new Pool({
-    user: 'me',
-    host :'localhost',
-    database: 'react_public_holidays',
-    password: 'prince.asamoah@29',
-    port: 5432
+  user: "me",
+  host: "localhost",
+  database: "react_public_holidays",
+  password: "prince.asamoah@29",
+  port: 5432,
 });
+
+const createUser = (req, res) => {
+  const { fullname, email, password } = req.body;
+  db.query(
+    "INSERT INTO users (fullname, email, password) VALUES($1, $2, $3)",
+    [fullname, email, password],
+    (error, results) => {
+      if (error) console.log(error);
+      res.sendStatus(200);
+    }
+  );
+};
