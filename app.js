74d041595f03228614dc061ec541c87108ac5d56
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-09 10:45:51 +0000

    Setup server to use cors, express json and express urlendcoded middlewares

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
