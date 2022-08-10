1d3787f4376307a9e4a3a3998c26d3225aa259e4
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-10 19:30:40 +0000

    Move createUser and getUserByEmail from db to signup controller

---
+++ controllers/signup.js
@@ -0,0 +1,53 @@
+const db = require('../db');
+const bcrypt = require("bcrypt");
+
+const createUser = (req, res) => {
+  const { fullname, email, password } = req.body;
+  const saltRounds = 10;
+  bcrypt.hash(password, saltRounds, function (error, hash) {
+    let passwordHash;
+    if (error) {
+      throw Error(error);
+    } else {
+      passwordHash = hash;
+      db.query(
+        "INSERT INTO users (fullname, email, password) VALUES($1, $2, $3)",
+        [fullname, email, passwordHash],
+        (error, results) => {
+          if (error.code === 23505) {
+            res.send(403).json({error: 'Email already exist, login or signup with another email!'});
+            throw error;
+          } else {
+            res.sendStatus(200);
+          }
+        }
+      );
+    }
+  });
+};
+
+const getUserByEmail = (req, res) => {
+  const { email } = req.query;
+  console.log(db);
+  db.query(
+    "SELECT * FROM users WHERE email = $1",
+    [email],
+    (error, results) => {
+      if (error) {
+        res.send(error);
+        console.log(error);
+      } else {
+        if (results.rows.length === 0) {
+          res.sendStatus(404);
+        } else {
+          res.status(200).json(results.rows[0]);
+        }
+      }
+    }
+  );
+};
+
+module.exports = {
+  createUser,
+  getUserByEmail
+}
