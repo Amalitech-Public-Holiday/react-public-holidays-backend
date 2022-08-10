361e08fd54b87a886af748c88e35d78460ee62fd
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-10 19:32:45 +0000

    Refactor database connection to use process.env.DATABASE_URL and export db client

--- db.js
+++ db.js
@@ -1,5 +1,4 @@
 const Client = require("pg").Client;
-const bcrypt = require('bcrypt');
 require("dotenv").config();
 
 const db = new Client({
@@ -9,52 +8,12 @@ const db = new Client({
   }
 });
 
-db.connect();
+db.connect(err => {
+  if (err) {
+    console.error('Database Connection Error:', err.stack);
+  } else {
+    console.log('Database Connected Successfully!')
+  }
+});
 
-const createUser = (req, res) => {
-  const { fullname, email, password } = req.body;
-  const saltRounds = 10;
-  bcrypt.hash(password, saltRounds, function(error, hash) {
-    let passwordHash;
-    if (error) {
-      throw Error(error);
-    } else {
-      passwordHash = hash;
-      db.query(
-        "INSERT INTO users (fullname, email, password) VALUES($1, $2, $3)",
-        [fullname, email, passwordHash],
-        (error, results) => {
-          if (error) {
-            throw Error(error);
-          } else {
-            res.sendStatus(200);
-          }
-        }
-      );
-    }
-  });
-};
-
-const getUserByEmail = (req, res) => {
-  const {email} = req.query;
-  db.query(
-    'SELECT * FROM users WHERE email = $1',
-    [email],
-    (error, results) => {
-      if (error) {
-        res.send(error);
-      } else {
-        if (results.rows.length === 0) {
-          res.sendStatus(404);
-        } else {
-          res.status(200).json(results.rows[0]);
-        }
-      }
-    }
-  );
-}
-
-module.exports = {
-  createUser,
-  getUserByEmail
-}
+module.exports = db;
