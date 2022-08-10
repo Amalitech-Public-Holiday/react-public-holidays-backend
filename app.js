1acba6e216103cbe17b3b42024f25836a97d69d3
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-10 19:33:37 +0000

    Import createUser and getUserByEmail from signup controller

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
-const { createUser, getUserByEmail } = require('./db');
+const { createUser, getUserByEmail } = require('./controllers/signup');
 
 app.use(cors());
 app.use(express.json());
