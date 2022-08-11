894cd66b84a7f12b54021ecbb6dc8e8d32e3fe91
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-11 09:25:12 +0000

    Implement logic to verify login credentials

--- controllers/login.js
+++ controllers/login.js
@@ -1,6 +1,28 @@
 const db = require("../db");
 const bcrypt = require("bcrypt");
 
-const getUserEmailAndPassword = () => {};
+const getUserEmailAndPassword = (req, res) => {
+    const {email, password} = req.body;
+    db.query("SELECT * FROM users WHERE email =$1", [email], (error, results) => {
+        if (error) console.log(error);
+        const user = results.rows[0];
+        bcrypt.compare(password, user.password, function(error, result) {
+            if (error) {
+                console.log(error);
+                res.status(500).json({error: 'Login unsuccessful, try again!'})
+            } else {
+                if (result) {
+                    res.status(200).json(
+                        {success: 'Login successful',
+                        user: user.fullname}
+                    );
+                } else {
+                    res.status(404).json({error: 'Email or password is incorrect, try again!'});
+                }
+            }
+        });
+    });
 
-module.exports = getUserEmailAndPassword;
+};
+
+module.exports = {getUserEmailAndPassword};
