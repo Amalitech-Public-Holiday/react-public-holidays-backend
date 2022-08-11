7486a0560bfdc17e29e922abf60a26238ae31bae
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-11 12:43:47 +0000

    Refactor to handle exceptions

--- controllers/login.js
+++ controllers/login.js
@@ -3,21 +3,27 @@ const bcrypt = require("bcrypt");
 
 const getUserEmailAndPassword = (req, res) => {
     const {email, password} = req.body;
-    db.query("SELECT * FROM users WHERE email =$1", [email], (error, results) => {
-        if (error) console.log(error);
-        const user = results.rows[0];
-        bcrypt.compare(password, user.password, function(error, result) {
-            if (error) {
-                console.log(error);
-                res.status(500).json({error500: 'Login unsuccessful due to server error, try again!'})
-            } else {
-                if (result) {
-                    res.status(200).json({username: user.fullname});
-                } else {
-                    res.status(404).json({error404: 'Email or password is incorrect, try again!'});
-                }
-            }
-        });
+    db.query("SELECT * FROM users WHERE email =$1", [email], (queryError, queryResults) => {
+        if (queryError) {
+          res.status(500).json({error500: 'Login unsuccessful due to server error, try again!'})
+          throw Error(queryError);
+        }
+        if (queryResults.rows.length !== 0) {
+          const user = queryResults.rows[0];
+          bcrypt.compare(password, user.password, function(bcryptError, bcryptResults) {
+              if (bcryptError) {
+                  res.status(500).json({error500: 'Login unsuccessful due to server error, try again!'});
+                  throw Error(bcryptError);
+              }
+              if (bcryptResults) {
+                  res.status(200).json({username: user.fullname});
+              } else {
+                res.status(404).json({error404: 'Email or password is incorrect, try again!'});
+              }
+          });
+        } else {
+            res.status(404).json({error404: 'Email or password is incorrect, try again!'});
+        }
     });
 
 };
