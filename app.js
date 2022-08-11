e15c8a58f50d595c7c29331406b209c89560cbac
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-11 09:27:00 +0000

    Create a post route to get and validate login details

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
 const { createUser, getUserByEmail } = require('./controllers/signup');
+const { getUserEmailAndPassword } = require("./controllers/login");
 
 app.use(cors());
 app.use(express.json());
@@ -15,6 +16,7 @@ app.get("/", (req, res) => {
 });
 
 app.get('/users', getUserByEmail);
+app.get('/users/login', getUserEmailAndPassword);
 app.post('/users/signup', createUser);
 
 app.listen(PORT, () => {
